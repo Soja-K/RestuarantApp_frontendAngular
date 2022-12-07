6ab950ce4112bb22b2382e3babcd4a3d837925a6
Soja-K <sojaayilur2001@gmail.com>
2022-12-07 23:10:56 +0530

    RestuarantApp_frontendAngular

---
+++ src/app/services/order-details.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderDetailsService {
+
+  constructor() { }
+
+  foodDetails = [
+    {
+      id:1,
+      foodName:"Paneer Grilled Sandwich",
+      foodDetails:"Pan-fried masala paneer.",
+      foodPrice:200,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wzfq7djolqxgdhghebbq"
+    },
+    {
+      id:2,
+      foodName:"Veggie Supreme",
+      foodDetails:"Onion| Green Capsicum|Mushroom |black olives , sweet corn , Red Paprika topped with Cheese",
+      foodPrice:369,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/sgbobtbxlojbtdnr2m5k"
+    },
+    {
+      id:3,
+      foodName:"Paneer Burger",
+      foodDetails:"panner",
+      foodPrice:149,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/xbeqlsck3p0kei53to7k"
+    },
+    {
+      id:4,
+      foodName:"Veg Masala Roll In Naan",
+      foodDetails:"A homely mix of mashed potato and veggies, seasoned with Indian spices. A filling sure to take you back to mom's kitchen.",
+      foodPrice:140,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/l2ng6gtge30sqaafqng7"
+    },
+    {
+      id:5,
+      foodName:"Indulgence Brownie",
+      foodDetails:"(Eggless) Indulge in richly decadent chocolate brownie crafted with love & topped with bitter-sweet chocolate that provides ultra-rich chocolate experience.",
+      foodPrice:105,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/iqlmbg1hlyc0dspdyzzv"
+    },
+    {
+      id:6,
+      foodName:"Oreo Cheesecake Ice Cream",
+      foodDetails:"This ice cream may look like plain chocolate, but it's not it's a super concentrated version of cookies 'n' cream. Its rich flavor and color come from crushed Oreo wafers ",
+      foodPrice:219,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wtj8esaeslvlscv8glj6"
+    },
+    {
+      id:7,
+      foodName:"Sandwich",
+      foodDetails:"Sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread, or more generally any dish wherein bread serves as a container or wrapper for another food type",
+      foodPrice:100,
+      foodImg:"../../../assets/pictures/sandwich.jpg"
+    },
+    {
+      id:8,
+      foodName:"Pizza",
+      foodDetails:"Pizza consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly usually, in a commercial setting, using a wood fired oven heated to a very high temperature and served hot .",
+      foodPrice:150,
+      foodImg:"../../../assets/pictures/pizzas.jpeg"
+    }
+  ]
+
+  
+
+
+
+
+
+}
